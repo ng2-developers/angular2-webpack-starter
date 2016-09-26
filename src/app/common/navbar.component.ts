3ad3b9db0faeb5e4d15f102a273ef0635b63395a
tsukhu <tsukhu@hcl.com>
2016-09-26 10:49:57 +0530

    Added login support

--- src/app/common/navbar.component.ts
+++ src/app/common/navbar.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import { AppStore } from '../common/models/appstore.model';
+import { User } from '../common/models/user.model';
+import { AuthService } from '../common/service/auth.service';
 
 @Component({
   selector: 'ctl-nav-bar',
   templateUrl: './navbar.template.html'
 })
 export class NavBarComponent {
-    ctlLogo = 'assets/img/logo-centurylink.png';
+   ctlLogo = 'assets/img/logo-centurylink.png';
+
+   user: Observable<User>;
+
+   constructor(public authService: AuthService,
+              public store: Store<AppStore>) {
+      this.user = authService.user;
+      authService.init();
+   //   this.user.subscribe(v => console.log(v));
+  }
+
+  login() {
+    this.authService.login('tsukhu@hcl.com','xxx');
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
 }
