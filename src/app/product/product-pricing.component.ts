623809ff0ccc2d4bd335470ffc957020436fe050
tsukhu <tsukhu@hcl.com>
2016-10-06 13:08:05 +0530

    minor updates

--- src/app/product/product-pricing.component.ts
+++ src/app/product/product-pricing.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { PriceVariant } from '../common/models/pricing.model';
 
 @Component({
   selector: 'product-pricing',
-  styleUrls: [ './product-pricing.component.css' ],
+  styleUrls: ['./product-pricing.component.css'],
   templateUrl: './product-pricing.component.html'
 })
 export class ProductPricingComponent {
-  
+  @Input() prices: PriceVariant[];
 }
