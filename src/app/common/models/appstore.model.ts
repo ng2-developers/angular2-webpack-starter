f8c58d1654ae6b6f68818547de50994b4901e1d6
tsukhu <tsukhu@hcl.com>
2016-10-06 15:22:47 +0530

    minor fix

--- src/app/common/models/appstore.model.ts
+++ src/app/common/models/appstore.model.ts
@@ -1,7 +1,9 @@
 import { User } from './user.model';
 import { Product } from './product.model';
+import { Pricing } from './pricing.model';
 
 export interface AppStore {
   user: User;
   product: Product;
+  prices: Pricing;
 };
