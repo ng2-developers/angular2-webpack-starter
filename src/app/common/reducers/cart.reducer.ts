5a13d98dfd5ee2dfda60bf5e858110e8f9d64a69
tsukhu <tsukhu@hcl.com>
2016-10-12 21:04:59 +0530

    reducer updates

--- src/app/common/reducers/cart.reducer.ts
+++ src/app/common/reducers/cart.reducer.ts
@@ -56,33 +56,19 @@ export const cartReducer: ActionReducer<ShoppingCart>
         }
         return state;
 
-      /*
-      return state.lineItems.map(lineItem => {
-        if (lineItem.productTemplateId === action.payload.productTemplateId) {
-          console.log('Matched' + lineItem.productTemplateId);
-          console.log(state.lineItems);
-          console.log(action.payload);
-          let lineItems = Object.assign({}, lineItem, {
-                locations: [...lineItem.locations, action.payload.location]}
-                );
-          return Object.assign({}, state, {
-            lineItems: lineItems });
-          }
-        }
-      );*/
-      /*     case 'UPDATE_LOCATION':
+      case 'UPDATE_LOCATION':
              return state.lineItems.map(item => {
                item.lineItems.map(location => {
                  return location.id === action.payload.id ?
                    Object.assign({}, location, action.payload) : item;
                });
              });
-           case 'DELETE_LOCATION':
+       case 'DELETE_LOCATION':
              return state.lineItems.map(item => {
                item.lineItems.filter(location => {
                  return location.id !== action.payload.id;
                });
-             });*/
+             });
       default:
         return state;
     }
